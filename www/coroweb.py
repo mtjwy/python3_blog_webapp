971edb0559111906633000a966d81c84ac3507e5
Yu Wu <wuyucsu@gmail.com>
2015-06-04 12:40:52 -0500

    add add_static()

--- www/coroweb.py
+++ www/coroweb.py
@@ -191,7 +191,10 @@ def add_routes(app, module_name):
 			if method and path:
 				add_route(app, fn)		
 		
-		
+def add_static(app):
+	path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+	app.router.add_static('/static/', path)
+	logging.info('add static %s => %s' % ('/static/', path))
 		
 		
 		
