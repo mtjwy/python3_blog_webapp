6eb95df5700defb11dea76695cf96ccdcd9a5ae3
Yu Wu <wuyucsu@gmail.com>
2015-06-03 21:19:00 -0500

    wrap SELECT

--- www/orm.py
+++ www/orm.py
@@ -24,3 +24,38 @@ def create_pool(loop, **kw):
 		loop = loop
 	)
 	
+#wrap SELECT query
+@asyncio.coroutine
+def select(sql, args, size = None):
+	log(sql, args)
+	global __pool
+	with (yield from __pool) as conn:
+		cur = yield from conn.cursor(aiomysql.DictCursor)
+		yield from cur.execute(sql.replace('?', '%s'), args or ())
+		if size:
+			rs = yield from cur.fetchmany(size)
+		else:
+			rs = yield from cur.fetchall()
+		yield from cur.close()
+		logging.info('row returned: %s' % len(rs))
+		return rs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
