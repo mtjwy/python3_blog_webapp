3674d56ca7cf842d264f1c8ac54da29f92c1bbeb
Yu Wu <wuyucsu@gmail.com>
2015-06-03 21:09:11 -0500

    coding the object-relational mapping ORM

---
+++ www/orm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yu'
+
+import asyncio, logging
+import aiomysql
+
+#create a global connection pool
+@asyncio.coroutine
+def create_pool(loop, **kw):
+	logging.info('create database connection pool..')
+	global __pool
+	__pool = yield from aiomysql.create_pool(
+		host = kw.get('host', 'localhost'),
+		port = kw.get('port', 3306),
+		user = kw['user'],
+		password = kw['password'],
+		db = kw['db'],
+		charset = kw.get('charset', 'utf-8'),
+		autocommit = kw.get('autocommit', True),
+		maxsize = kw.get('maxsize', 10),
+		minsize = kw.get('minsize', 1),
+		loop = loop
+	)
+	
