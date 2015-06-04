86932a2c22798b09e08ed437f0205c75df47408b
Yu Wu <wuyucsu@gmail.com>
2015-06-04 12:24:22 -0500

    write a add_route function for register a URL processing function

--- www/coroweb.py
+++ www/coroweb.py
@@ -148,7 +148,17 @@ class RequestHandler(object):
 			return dict(error=e.error, data=e.data, message=e.message)
 					
 		
-		
+#write a add_route function for register a URL processing function
+def add_route(app, fn):
+	method = getattr(fn, '__method__', None)
+	path = getattr(fn, '__route__', None)
+	if path is None or method is None:
+		raise ValueError('@get or @post not defined in %s.' %str(fn))
+	if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+		fn = asyncio.coroutine(fn)
+	logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+	app.router.add_route(method, path, RequestHandler(app, fn))
+
 		
 		
 		
