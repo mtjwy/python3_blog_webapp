9d730d2c96c53d2e1f5bae774d82697c5b42e407
Yu Wu <wuyucsu@gmail.com>
2015-06-04 18:24:35 -0500

    finish blog template

--- www/handlers.py
+++ www/handlers.py
@@ -11,10 +11,24 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
-@get('/')
+
+'''
 def index(request):
     users = yield from User.findAll()
     return {
         '__template__': 'test.html',
         'users': users
+    }
+'''
+@get('/')
+def index(request):
+    summary = 'Life is a short day; but it is a working day. Activity may lead to evil, but inactivity cannot lead to good.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Java', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
