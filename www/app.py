ecd98764a2538b020883318fc2db6fe9a201baba
Yu Wu <wuyucsu@gmail.com>
2015-06-04 12:38:21 -0500

    in app.py, adding middleware、jinja2 template and auto register support

--- www/app.py
+++ www/app.py
@@ -12,13 +12,23 @@ import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
+
+
+
 def index(request):#create a request handler. # accepts only request parameters of type Request and returns Response instance.
 	return web.Response(body = b'<h1>Awesome Blog</h1>')
 	
 @asyncio.coroutine
 def init(loop):
-	app = web.Application(loop = loop)#create a Application instance 
-	app.router.add_route('GET', '/', index)#register handler in the application’s router pointing HTTP method, path and handler.
+	#in app.py, adding middleware、jinja2 template and auto register support：
+	yield from orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='123', db='blogWeb')
+	app = web.Application(loop = loop, middlewares = [
+		logger_factory, response_factory
+	])#create a Application instance 
+	init_jinja2(app, filters=dict(datetime=datetime_filter))
+	add_routes(app, 'handlers')
+	add_static(app)
+	#app.router.add_route('GET', '/', index)#register handler in the application’s router pointing HTTP method, path and handler.
 	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 5000)#Create a TCP server
 	logging.info('server started at http://127.0.0.1:5000...')
 	return srv
