351b5fdf17ae4119ca5d772dccaa87e5abec5ae1
Yu Wu <wuyucsu@gmail.com>
2015-06-04 12:29:13 -0500

    add add_routes() to auto register all the functions in handlers

--- www/coroweb.py
+++ www/coroweb.py
@@ -160,7 +160,36 @@ def add_route(app, fn):
 	app.router.add_route(method, path, RequestHandler(app, fn))
 
 		
-		
+'''
+convert many add_route() call：
+
+add_route(app, handles.index)
+add_route(app, handles.blog)
+add_route(app, handles.create_comment)
+...
+
+into auto scan:
+
+# auto register all the functions in handlers :
+add_routes(app, 'handlers')
+'''
+
+def add_routes(app, module_name):
+	n = module_name.rfind('.')
+	if n == (-1):
+		mod = __import__(module_name, globals(), locals())
+	else:
+		name = module_name[n+1:]
+		mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+	for attr in dir(mod):
+		if attr.startswith('_'):
+			continue
+		fn = getattr(mod, attr)
+		if callable(fn):
+			method = getattr(fn, '__method__', None)
+			path = getattr(fn, '__route__', None)
+			if method and path:
+				add_route(app, fn)		
 		
 		
 		
