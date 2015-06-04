14581fac78ca91bd34d66a2c236c2ccd7203c81d
Yu Wu <wuyucsu@gmail.com>
2015-06-04 12:50:55 -0500

    add response_factory()

--- www/app.py
+++ www/app.py
@@ -21,6 +21,46 @@ def logger_factory(app, handler):
 		return (yield from handler(request))
 	return logger
 
+#response middleware is to convert return value to web.Response object，so as to meet the requirement of aiohttp：
+@asyncio.coroutine
+def response_factory(app, handler):
+	@asyncio.coroutine
+	def response(request):
+		logging.info('Response handler...')
+		r = yield from handler(request)
+		if isinstance(r, web.StreamResponse):
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet-stream'
+			return resp
+		if isinstance(r, str):
+			if r.startswith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		if isinstance(r, dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else:
+				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		if isinstance(r, int) and t >= 100 and t < 600:
+			return web.Response(t)
+		if isinstance(r, tuple) and len(r) == 2:
+			t, m = r
+			if isinstance(t, int) and t >= 100 and t < 600:
+				return web.Response(t, str(m))
+		# default:
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+	return response
 
 def index(request):#create a request handler. # accepts only request parameters of type Request and returns Response instance.
 	return web.Response(body = b'<h1>Awesome Blog</h1>')
